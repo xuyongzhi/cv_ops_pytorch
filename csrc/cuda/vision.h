aa1d5032909eea752534157191bd4d7b7d6886e8
xuyongzhi <buaaxyz@yeah.net>
2019-04-11 21:58:41 +1000

    add ROIAlignRotated_cuda.cu not tested yet

--- csrc/cuda/vision.h
+++ csrc/cuda/vision.h
@@ -61,3 +61,22 @@ at::Tensor nms_cuda(const at::Tensor boxes, float nms_overlap_thresh);
 at::Tensor compute_flow_cuda(const at::Tensor& boxes,
                              const int height,
                              const int width);
+
+at::Tensor ROIAlignRotated_forward_cuda(const at::Tensor& input,
+                                 const at::Tensor& rois,
+                                 const float spatial_scale,
+                                 const int pooled_height,
+                                 const int pooled_width,
+                                 const int sampling_ratio);
+
+at::Tensor ROIAlignRotated_backward_cuda(const at::Tensor& grad,
+                                  const at::Tensor& rois,
+                                  const float spatial_scale,
+                                  const int pooled_height,
+                                  const int pooled_width,
+                                  const int batch_size,
+                                  const int channels,
+                                  const int height,
+                                  const int width,
+                                  const int sampling_ratio);
+
--- csrc/vision.cpp
+++ csrc/vision.cpp
@@ -3,6 +3,7 @@
 #include "ROIAlign.h"
 #include "ROIPool.h"
 #include "SigmoidFocalLoss.h"
+#include "ROIAlignRotated.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("nms", &nms, "non-maximum suppression");
@@ -12,4 +13,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("roi_pool_backward", &ROIPool_backward, "ROIPool_backward");
   m.def("sigmoid_focalloss_forward", &SigmoidFocalLoss_forward, "SigmoidFocalLoss_forward");
   m.def("sigmoid_focalloss_backward", &SigmoidFocalLoss_backward, "SigmoidFocalLoss_backward");
+  m.def("roi_align_rotated_forward", &ROIAlignRotated_forward, "ROIAlignRotated_forward");
+  m.def("roi_align_rotated_backward", &ROIAlignRotated_backward, "ROIAlignRotated_backward");
 }
